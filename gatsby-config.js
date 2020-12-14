5e4623da2852e36b32526ce823c69318d6c677cb
Fabian Tjoe-A-On <fabian@chainels.com>
2020-12-14 11:08:05 +0100

    Test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+const ffprobe = require("@ffprobe-installer/ffprobe");
+const ffprobe = require("@ffprobe-installer/ffprobe");
+console.log(ffprobe.path, ffprobe.version);
+
 module.exports = {
   siteMetadata: {
     title: `Fabian Tjoe-A-On`,
